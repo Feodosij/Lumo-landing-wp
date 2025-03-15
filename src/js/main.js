1f3de67faaa21e41067e6ec5a7c9d17836193bc1
Феодосий Шаповалов <feodosii@Noutbuk-Feodosij.local>
2025-03-15 22:55:16 +0000

    refactor

--- src/js/main.js
+++ src/js/main.js
@@ -17,4 +17,21 @@ document.addEventListener("DOMContentLoaded", function () {
             body.classList.remove("lock");
 		});
 	});
+
+	const scrollToTop = document.getElementById("scrollToTop");
+
+	window.addEventListener("scroll", function () {
+		if(window.scrollY > 300) {
+			scrollToTop.classList.add("visible");
+		} else {
+			scrollToTop.classList.remove("visible");
+		}
+	});
+
+	scrollToTop.addEventListener("click", function () {
+		window.scrollTo({
+			top: 0
+		});
+	});
+
 });
